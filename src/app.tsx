8c11773741eb92e4cc4f201f238c55969d47cb10
Henning Mosand Stephansen <henningmosand@outlook.com>
2016-10-10 23:22:59 +0200

    Adds a small comment

--- src/app.tsx
+++ src/app.tsx
@@ -25,6 +25,14 @@ interface State {
 
 }
 
+/**
+ * The main component of the app.. Called App..
+ * 
+ * Component is extendig with Props, and State, whic in this case
+ * is two empty interfaces. Since it's Typescript it would be wise
+ * to try at least to define what kind of props and state that should
+ * be dealt with in components. Otherwise you can still use the any keyword
+ */
 export default class App extends Component<Props, State> {
   render() {
     return (
